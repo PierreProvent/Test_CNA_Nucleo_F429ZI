08869cf57cac70b1378bd69e9fb579cd2be5736c
pierreprovent <PierreProvent@users.noreply.github.com>
2020-10-07 13:03:58 +0000

    Programme test pour CNA cours ELE118 Cnam

--- main.cpp
+++ main.cpp
@@ -1,38 +1,17 @@
 #include "mbed.h"
 
-extern void Sauve_USB(float *,int ) ; 
-extern Serial pc ;
+Serial pc(USBTX, USBRX);
 
-#define FECH 20000 // Fréquence d'échantillonnage souhaitée
-                   // Maximum 40 kHz  
-#define TAILLE_TAB (FECH/10) // Pour stocker 1/10 s du signal
-                             // Donc fmin signal = 10 Hz
+AnalogIn Ve(PA_3); // CAN sur A0 connecteur Arduino
+AnalogOut Vs(PA_5); // CNA sur D13 connecteur Arduino
 
-Ticker acquisition_CAN ;
-AnalogIn entree(PC_3); // A2 connecteur Arduino
-DigitalIn button(USER_BUTTON);
-
-float tab[TAILLE_TAB] ;
-
-void acquisition() {
-    static int i = 0 ;
-    tab[i] = 3.3f*entree.read(); // Conversion en volts
-    i++;
-    if (i == TAILLE_TAB) i = 0 ;
-}
-
-int main(void)
+int main()
 {
-    pc.printf("\033[2J"); // Sequence escape qui efface la console
-    pc.printf("Acquisition d'un signal sur broche PC_3(A2)\n");
-    pc.printf("Et enregistrement sur clef USB\n"); 
-    pc.printf("Frequence d'echantillonnage actuelle = %d Hz\n",FECH); 
-    pc.printf("Veuillez connecter un signal sur cette broche \n");
-    pc.printf("Appuyez sur le bouton USER de la carte pour demarrer l'acquisition\n");
-    while (button.read() == 0) ;
-    acquisition_CAN.attach_us(&acquisition,1000000/FECH); 
-    wait(0.2) ;   // Attendre au moins 1/10 seconde pour remplir le buffer des échantillons prélevés sur PC_3
-    acquisition_CAN.detach() ; // Stopper l'acquisition des échantillons
-    Sauve_USB(tab,TAILLE_TAB);
-    while(1) ;
+    pc.printf("Test du convertisseur numerique/analogique\n");
+    pc.printf("Relier D13 (CNA) a A0 (CAN) du connecteur Arduino\n");
+    Vs.write(0.5);
+    pc.printf("Valeur envoyee sur D13 = %f \n", 0.5);
+    pc.printf("Valeur lue sur A0     = %f \n", Ve.read());
+    while(1) {
+    }
 }
